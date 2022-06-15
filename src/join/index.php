241d1bf30236d750cc0a562e9373cb9f5878a6a0
UrakamiTakuya <urakami_t@mediaxis.jp>
2022-06-15 09:47:24 +0900

    入力が完了するとcheck.phpに遷移する。 session_startを使ってformで入力した内容をcheck.phpに渡す

--- src/join/index.php
+++ src/join/index.php
@@ -1,9 +1,48 @@
 <?php
-$form = [];
+// セッションを使ってcheck.phpにフォームで記載した内容を持っていく
+session_start();
+
+$form = [
+    'name' => '',
+    'email' => '',
+    'password' => ''
+];
 $error = [];
-$form['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-if ($form['name'] === '') {
-    $error['name'] = 'blank';
+
+// フォームの内容をチェック
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $form['name'] = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    if ($form['name'] === '') {
+        $error['name'] = 'blank';
+    }
+
+    $form['email'] = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    if ($form['email'] === '') {
+        $error['email'] = 'blank';
+    }
+
+    $form['password'] = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+    if ($form['password'] === 0) {
+        $error['password'] = 'blank';
+    } else if (strlen($form['password'] < 4)) {
+        $error['password'] = 'not enough length';
+    }
+
+    // 画像のチェック
+    $image = $_FILES['image'];
+    if ($image['name'] !== '' && $image['error'] === 0) {
+        // なんのファイルがアップロードされたのかわかる。（例 imge/png）
+        $type = mime_content_type($image['tmp_name']);
+        if ($type !== 'image/png' && $type !== 'image/jpeg') {
+            $error['image'] = 'not type';
+        }
+    }
+
+    if (empty($error)) {
+        $_SESSION['form'] = $form;
+        header('Location: check.php');
+        exit();
+    }
 }
 ?>
 
@@ -32,26 +71,34 @@ if ($form['name'] === '') {
             <dl>
                 <dt>ニックネーム<span class="required">必須</span></dt>
                 <dd>
-                    <input type="text" name="name" size="35" maxlength="255" value=""/>
+                    <input type="text" name="name" size="35" maxlength="255" value=<?= $form['name']; ?>>
                     <?php if (isset($error['name']) && $error['name'] === 'blank'): ?>
                         <p class="error">* ニックネームを入力してください</p>
                     <?php endif; ?>
                 </dd>
                 <dt>メールアドレス<span class="required">必須</span></dt>
                 <dd>
-                    <input type="text" name="email" size="35" maxlength="255" value=""/>
-                    <p class="error">* メールアドレスを入力してください</p>
-                    <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
+                    <input type="text" name="email" size="35" maxlength="255" value=<?= $form['email']; ?>>
+                    <?php if (isset($error['email']) && $error['email'] === 'blank'): ?>
+                        <p class="error">* メールアドレスを入力してください</p>
+                    <?php endif; ?>
+                        <p class="error">* 指定されたメールアドレスはすでに登録されています</p>
                 <dt>パスワード<span class="required">必須</span></dt>
                 <dd>
-                    <input type="password" name="password" size="10" maxlength="20" value=""/>
-                    <p class="error">* パスワードを入力してください</p>
-                    <p class="error">* パスワードは4文字以上で入力してください</p>
+                    <input type="password" name="password" size="10" maxlength="20" value=<?= $form['password']; ?>>
+                    <?php if (isset($error['password']) && $error['password'] === 'blank'): ?>
+                        <p class="error">* パスワードを入力してください</p>
+                    <?php endif; ?>
+                    <?php if (isset($error['password']) && $error['password'] === 'not enough length'): ?>
+                        <p class="error">* パスワードは4文字以上で入力してください</p>
+                    <?php endif; ?>
                 </dd>
                 <dt>写真など</dt>
                 <dd>
                     <input type="file" name="image" size="35" value=""/>
-                    <p class="error">* 写真などは「.png」または「.jpg」の画像を指定してください</p>
+                    <?php if (isset($error['image']) && $error['image'] === 'not type'): ?>
+                        <p class="error">* 写真などは「.png」または「.jpg」の画像を指定してください</p>
+                    <?php endif; ?>
                     <p class="error">* 恐れ入りますが、画像を改めて指定してください</p>
                 </dd>
             </dl>
